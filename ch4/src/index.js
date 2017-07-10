e4bdfa826e4d5448f3c04b586a9a266fbe042355
Marc Garreau <marcdgarreau@gmail.com>
2017-07-09 22:31:44 -0600

    Fixes DevTools configuration

--- ch4/src/index.js
+++ ch4/src/index.js
@@ -5,7 +5,7 @@ import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
-import { devToolsEnhancer } from 'redux-devtools-extension';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import tasksReducer from './reducers';
 import App from './App';
 import './index.css';
@@ -18,15 +18,14 @@ const rootReducer = (state = {}, action) => {
 
 const store = createStore(
   rootReducer,
-  applyMiddleware(thunk),
-  devToolsEnhancer(),
+  composeWithDevTools(applyMiddleware(thunk))
 );
 
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById('root'),
+  document.getElementById('root')
 );
 
 if (module.hot) {
@@ -34,7 +33,7 @@ if (module.hot) {
     const NextApp = require('./App').default;
     ReactDOM.render(
       <Provider store={store}><NextApp /></Provider>,
-      document.getElementById('root'),
+      document.getElementById('root')
     );
   });
 
