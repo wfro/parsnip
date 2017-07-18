4ccb2f7196ad75bb9c8a558a9edc84ad7498ba7c
Will Faurot <wfaurot@gmail.com>
2017-07-18 09:10:26 -0700

    After adding analytics

---
+++ ch5/src/index.js
@@ -0,0 +1,48 @@
+import registerServiceWorker from './registerServiceWorker';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import tasksReducer from './reducers';
+import logger from './middlewares/logger';
+import analytics from './middlewares/analytics';
+import App from './App';
+import './index.css';
+
+const rootReducer = (state = {}, action) => {
+  return {
+    tasks: tasksReducer(state.tasks, action),
+  };
+};
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk, logger, analytics)),
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'),
+);
+
+if (module.hot) {
+  module.hot.accept('./App', () => {
+    const NextApp = require('./App').default;
+    ReactDOM.render(
+      <Provider store={store}><NextApp /></Provider>,
+      document.getElementById('root'),
+    );
+  });
+
+  module.hot.accept('./reducers', () => {
+    const nextRootReducer = require('./reducers').default;
+    store.replaceReducer(nextRootReducer);
+  });
+}
+
+registerServiceWorker();
