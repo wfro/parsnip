26cdbe2b12b3b65fc48ad4af7bbb20c24a926586
Will Faurot <will.faurot@instacart.com>
2017-08-24 14:57:30 -0700

    Update createTaskSucceeded action to use meta/analytics

--- ch5/src/actions/index.js
+++ ch5/src/actions/index.js
@@ -45,6 +45,14 @@ function createTaskSucceeded(task) {
     payload: {
       task,
     },
+    meta: {
+      analytics: {
+        event: 'create_task',
+        data: {
+          id: task.id,
+        },
+      },
+    },
   };
 }
 
