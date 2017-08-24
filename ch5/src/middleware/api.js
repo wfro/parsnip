350c0cc8a85165fce52363a9c1f3e9498176a743
Will Faurot <will.faurot@instacart.com>
2017-08-24 15:21:54 -0700

    Update api middleware to handle non-gets

--- ch5/src/middleware/api.js
+++ ch5/src/middleware/api.js
@@ -4,17 +4,19 @@ const API_BASE_URL = 'http://localhost:3001';
 
 export const CALL_API = 'CALL_API';
 
-function makeCall(endpoint) {
+function makeCall({ endpoint, method = 'GET', body }) {
   const url = `${API_BASE_URL}${endpoint}`;
 
-  return axios
-    .get(url)
-    .then(resp => {
-      return resp;
-    })
-    .catch(err => {
-      return err;
-    });
+  const params = {
+    method: method,
+    url,
+    data: body,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
+
+  return axios(params).then(resp => resp).catch(err => err);
 }
 
 const apiMiddleware = store => next => action => {
@@ -27,7 +29,11 @@ const apiMiddleware = store => next => action => {
 
   next({ type: requestStartedType });
 
-  return makeCall(callApi.endpoint).then(
+  return makeCall({
+    method: callApi.method,
+    body: callApi.body,
+    endpoint: callApi.endpoint,
+  }).then(
     response =>
       next({
         type: successType,
