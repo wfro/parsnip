e8e50c8fc11328f3038f5861f79200f32ef72a17
Will Faurot <will.faurot@instacart.com>
2017-08-24 15:16:15 -0700

    Start filling in more functionality for api middleware

--- ch5/src/middleware/api.js
+++ ch5/src/middleware/api.js
@@ -1,10 +1,31 @@
+import axios from 'axios';
+
+const API_BASE_URL = 'http://localhost:3001'; //#A
+
 export const CALL_API = 'CALL_API';
+
+function makeCall(endpoint) {
+  const url = `${API_BASE_URL}${endpoint}`; //#B
+
+  return axios
+    .get(url)
+    .then(resp => {
+      return resp; //#C
+    })
+    .catch(err => {
+      return err; //#C
+    });
+}
 
 const apiMiddleware = store => next => action => {
   const callApi = action[CALL_API];
   if (typeof callApi === 'undefined') {
     return next(action);
   }
+
+  const [requestStartedType, successType, failureType] = callApi.types;
+
+  next({ type: requestStartedType });
 };
 
 export default apiMiddleware;
