8e09b4aa4d8df06deac2aec742ea707fb4978d38
Will Faurot <will.faurot@instacart.com>
2017-08-24 15:17:18 -0700

    Make the AJAX call in api middleware

--- ch5/src/middleware/api.js
+++ ch5/src/middleware/api.js
@@ -1,19 +1,19 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:3001'; //#A
+const API_BASE_URL = 'http://localhost:3001';
 
 export const CALL_API = 'CALL_API';
 
 function makeCall(endpoint) {
-  const url = `${API_BASE_URL}${endpoint}`; //#B
+  const url = `${API_BASE_URL}${endpoint}`;
 
   return axios
     .get(url)
     .then(resp => {
-      return resp; //#C
+      return resp;
     })
     .catch(err => {
-      return err; //#C
+      return err;
     });
 }
 
@@ -26,6 +26,19 @@ const apiMiddleware = store => next => action => {
   const [requestStartedType, successType, failureType] = callApi.types;
 
   next({ type: requestStartedType });
+
+  return makeCall(callApi.endpoint).then(
+    response =>
+      next({
+        type: successType,
+        response,
+      }),
+    error =>
+      next({
+        type: failureType,
+        error: error.message,
+      })
+  );
 };
 
 export default apiMiddleware;
