23addf60d6664741145e506ed5f3c82a3f5f81af
Will Faurot <will.faurot@instacart.com>
2017-12-27 06:02:19 -0800

    better intro

--- ch2/src/App.js
+++ ch2/src/App.js
@@ -4,7 +4,17 @@ class App extends Component {
   render() {
     return (
       <div className="main-content">
-        Redux in Action
+        <div style={{textAlign: 'center', maxWidth: 660, margin: '0 auto'}}>
+          <h1>
+            Welcome to Redux in Action! Let's do this thing.
+          </h1>
+          <h2>
+            If you used the easy-install script, you're all set with a local environment bootstrapped with Create React App. The repo also includes a few things like styles that will come handy in the book.
+          </h2>
+          <h2>
+            Head to chapter 1 for a general intro to Redux, and then jump into the code starting with chapter 2.
+          </h2>
+        </div>
       </div>
     );
   }
--- ch2/src/index.js
+++ ch2/src/index.js
@@ -2,16 +2,11 @@ import registerServiceWorker from './registerServiceWorker';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import tasks from './reducers';
 import App from './App';
 import './index.css';
 
-const store = createStore(tasks);
-
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <App />,
   document.getElementById('root')
 );
 
